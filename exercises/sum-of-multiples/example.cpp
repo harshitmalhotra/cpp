d998b5f3be3429c42016bce93ce5e2aea7069a01
harshitmalhotra <42599967+harshitmalhotra@users.noreply.github.com>
2019-10-31 22:36:42 +0530

    Update example.cpp
    1. Indentation added in the inner loops
    2. Added namespace std,  instead of individually using  "std::"
    3. Removed Unnecessary blank lines

--- exercises/sum-of-multiples/example.cpp
+++ exercises/sum-of-multiples/example.cpp
@@ -1,15 +1,20 @@
 #include "sum_of_multiples.h"
 
+using namespace std;
+
 namespace sum_of_multiples
 {
 
-int to(std::initializer_list<int> const& multiples, int num)
+int to(initializer_list<int> const& multiples, int num)
 {
     int sum = 0;
-    for (int i = 0; i < num; ++i) {
-        for (const int multiple : multiples) {
-            if (i % multiple == 0) {
-                sum += i;
+    for (int i = 0; i < num; ++i) 
+    {
+        for (const int multiple : multiples) 
+        {
+            if (i % multiple == 0) 
+            {
+                sum = sum + i;
                 break;
             }
         }
